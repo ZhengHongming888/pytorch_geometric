f583a30fe214eb38c8f724d44a87e296a6fc7d9b
ZhengHongming888 <hongming.zheng@intel.com>
2023-05-18 19:37:38 -0700

    modify LocalFeatureStore with id2index

--- torch_geometric/data/local_feature_store.py
+++ torch_geometric/data/local_feature_store.py
@@ -6,16 +6,16 @@ from torch import Tensor
 from torch_geometric.data import FeatureStore, TensorAttr
 from torch_geometric.typing import FeatureTensorType
 
-
 class LocalFeatureStore(FeatureStore):
-    def __init__(self,
-        id2index: Optional[torch.Tensor] = None
-        ):
+    def __init__(self):
 
         self.store: Dict[Tuple[str, str], Tensor] = {}
 
-        # save the the mapping from global sampled node ids to local node ids
-        self.id2index: Optional[torch.Tensor] = id2index
+        # save the global node/edge ids
+        self.global_idx: Dict[Tuple[str, str], Tensor] = {}
+
+        # save the the mapping from global sampled node/edge ids to local feature index
+        self.id2index: Dict[Tuple[str, str], Tensor] = {}
 
         super().__init__()
 
@@ -23,8 +23,33 @@ class LocalFeatureStore(FeatureStore):
     def key(attr: TensorAttr) -> str:
         return (attr.group_name, attr.attr_name)
 
-    def init_id2index(self, id2index: torch.Tensor):
-        self.id2index = id2index
+    def set_global_ids(self, global_ids: Tensor, group_name: str, attr_name: str):
+        key_name = (group_name, attr_name)
+        self.global_idx[key_name] = global_ids
+
+    def set_global_ids_plus_id2index(self, global_ids: Tensor, group_name: str, attr_name: str):
+        key_name = (group_name, attr_name)
+        self.global_idx[key_name] = global_ids
+
+        max_id = torch.max(global_ids).item()
+        id2idx = torch.zeros(max_id + 1, dtype=torch.int64)
+        id2idx[global_ids] = torch.arange(global_ids.size(0), dtype=torch.int64)
+
+        self.id2index[key_name] = id2idx
+
+    def set_id2index(self, group_name: str, attr_name: str):
+        key_name = (group_name, attr_name)
+
+        global_ids = self.global_idx.get(key_name)
+        max_id = torch.max(global_ids).item()
+        id2idx = torch.zeros(max_id + 1, dtype=torch.int64)
+        id2idx[global_ids] = torch.arange(global_ids.size(0), dtype=torch.int64)
+
+        self.id2index[key_name] = id2idx
+
+    def get_global_ids(self, group_name: str, attr_name: str) -> Tensor:
+        key_name = (group_name, attr_name)
+        return self.global_idx.get(key_name)
 
     def _put_tensor(self, tensor: FeatureTensorType, attr: TensorAttr) -> bool:
         index = attr.index
